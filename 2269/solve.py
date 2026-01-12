67375e1df03479d0b161c07017abe8010dcb9f52
vanitas1209 <hajincreeper@gmail.com>
2026-01-12 11:39:09 +0900

    260112_7

--- 2269/solve.py
+++
@@ -1,9 +0,0 @@
-def caesar_decode(text, shift):
-    result = ""
-    for char in text:
-        if char.isalpha():
-            base = ord('A') if char.isupper() else ord('a')
-            result += chr((ord(char) - base - shift) % 26 + base)
-        else:
-            result += char
-    return result
